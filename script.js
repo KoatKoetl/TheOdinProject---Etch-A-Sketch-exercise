7eb04083256f6e00a3721eb18a55bc2deac7bdf6
Suschevici Stanislav <stas20027@outlook.com>
2023-07-25 11:30:40 +0300

    Rebuild a logic to work correct

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-let gridSize = 16;
-
-function  createOneColumn(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
+function  createOneColumn() {
+    for (let i = 1; i <= 16; i++) {
       // Create a div element:
       const height = document.createElement("div");
       height.classList.add("lineOfSquares");
       //add element to grid
       document.getElementById("fullSquare").appendChild(height);
     }
-  for (let i = 0; i < gridSize; i++) {
-    // Create a div element:
-    const line = document.createElement("div");
+};
+function createRow(){
+    for (let i = 1; i <= 16; i++) {
+        // Create a div element:
+        const line = document.createElement("div");
+    
+        //add element to grid
+        const allElements = document.getElementsByClassName("lineOfSquares");
+    
+        allElements[i].appendChild(line);
+      }
+};
 
-    //add element to grid
-    const allElements = document.getElementsByClassName("lineOfSquares");
 
-    allElements[i].appendChild(line);
-  }
-}
-
-function createGrid(gridSize) {
-    for(let i = 0; i < gridSize; i++){
-        createOneColumn(gridSize);
+function createGrid() {
+    createOneColumn();
+    for(let i = 1; i <= 16; i++){
+        createRow();
     }
-}
+};
 
-createGrid(gridSize);
+createGrid();
+
+// function hoverColorEffect (){
+//     function mouseOver(){
+//         document.getElementsByClassName(".lineOfSquares")
+//     }
+// }
